5ae4d153bad0be4c2017841039b429bd6e452fbb
Shawn McNaughton <shawngmc@gmail.com>
2017-09-10 16:59:09 -0400

    Reworked Zone Definitions

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ var generateIconURL = function (type, subtype) {
 };
 
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 var getRandomColor = function () {
@@ -24,27 +24,27 @@ var generatePopupWithSearchIcons = function (objDesc, objType) {
         links.google = links.google + "+" + objType.replace(" ", "+");
     }
     span.innerHTML = '<span>Task: ' + objDesc + '<br><a href="' + links.youtube + '" target="_blank"><img src="images/yt_icon_rgb.png" height="24" width="34" /></a><a href="' + links.google + '" target="_blank"><img src="images/google_icon.png" height="24" width="24" /></a></span>';
-    return span;   
+    return span;
 };
 
 var getMergedFloorData = function () {
     return Promise.all([fetch("https://api.guildwars2.com/v2/continents/1/floors/0"), fetch("https://api.guildwars2.com/v2/continents/1/floors/1")])
         .then(function (floorDataRequestResponses) {
-           return Promise.all([floorDataRequestResponses[0].text(), floorDataRequestResponses[1].text()]);
+            return Promise.all([floorDataRequestResponses[0].text(), floorDataRequestResponses[1].text()]);
         })
         .then(function (floorDataRawResponses) {
-           // Hack - Force Gilded Hollow Data in!
-           var floorData = [];
-           floorData[0] = JSON.parse(floorDataRawResponses[0]);
-           floorData[1] = JSON.parse(floorDataRawResponses[1]);
-           
-           // Data fix - force Gilded Hollow into floor 1;
-           floorData[1].regions[10].maps[1068] = floorData[0].regions[10].maps[1068];
-           
-           // Data fix - change top of Dry Top to Bottom of Silverwastes - there is MUCH less overlap than it thinks!
-           floorData[1].regions[11].maps[988].continent_rect[0][1] = floorData[1].regions[11].maps[1015].continent_rect[1][1];
-           
-           return floorData[1];
+            // Hack - Force Gilded Hollow Data in!
+            var floorData = [];
+            floorData[0] = JSON.parse(floorDataRawResponses[0]);
+            floorData[1] = JSON.parse(floorDataRawResponses[1]);
+
+            // Data fix - force Gilded Hollow into floor 1;
+            floorData[1].regions[10].maps[1068] = floorData[0].regions[10].maps[1068];
+
+            // Data fix - change top of Dry Top to Bottom of Silverwastes - there is MUCH less overlap than it thinks!
+            floorData[1].regions[11].maps[988].continent_rect[0][1] = floorData[1].regions[11].maps[1015].continent_rect[1][1];
+
+            return floorData[1];
         });
 };
 
@@ -104,55 +104,61 @@ icons.task = L.icon({
     popupAnchor: [-3, -3]
 });
 
-var maps = {
-    26: "Dredgehaunt Cliffs",
-    27: "Lornar's Pass",
-    28: "Wayfarer Foothills",
-    29: "Timberline Falls",
-    30: "Frostgorge Sound",
-    31: "Snowden Drifts",
-    326: "Hoelbrak",
-    19: "Plains of Ashford",
-    20: "Blazeridge Steppes",
-    21: "Fields of Ruin",
-    22: "Fireheart Rise",
-    25: "Iron Marches",
-    32: "Diessa Plateau",
-    218: "Black Citadel",
-    51: "Straits of Devastation",
-    62: "Cursed Shore",
-    65: "Malchor's Leap",
-    1203: "Siren's Landing",
-    15: "Queensdale",
-    17: "Harathi Hinterlands",
-    18: "Divinity's Reach",
-    23: "Kessex Hills",
-    24: "Gendarran Fields",
-    50: "Lion's Arch",
-    73: "Bloodtide Coast",
-    873: "Southsun Cove",
-    1185: "Lake Doric",
-    34: "Caledon Forest",
-    35: "Metrica Province",
-    54: "Brisban Wildlands",
-    91: "The Grove",
-    139: "Rata Sum",
-    39: "Mount Maelstrom",
-    53: "Sparkfly Fen",
-    1041: "Dragon's Stand",
-    1043: "Auric Basin",
-    1045: "Tangled Depths",
-    1052: "Verdant Brink",
-    1068: "Gilded Hollow",
-    1069: "Lost Precipice",
-    1165: "Bloodstone Fen",
-    988: "Dry Top",
-    1015: "The Silverwastes",
-    1210: "Crystal Oasis",
-    1175: "Ember Bay",
-    1195: "Draconis Mons"
+var worldZones = {
+    26: { "name": "Dredgehaunt Cliffs", "type":  "Story", "zoneCategory": "GW2" },
+    27: { "name": "Lornar's Pass", "type":  "Story", "zoneCategory": "GW2" },
+    28: { "name": "Wayfarer Foothills", "type":  "Story", "zoneCategory": "GW2" },
+    29: { "name": "Timberline Falls", "type":  "Story", "zoneCategory": "GW2" },
+    30: { "name": "Frostgorge Sound", "type":  "Story", "zoneCategory": "GW2" },
+    31: { "name": "Snowden Drifts", "type":  "Story", "zoneCategory": "GW2" },
+    326: { "name": "Hoelbrak", "type": "City", "zoneCategory": "GW2" },
+    19: { "name": "Plains of Ashford", "type":  "Story", "zoneCategory": "GW2" },
+    20: { "name": "Blazeridge Steppes", "type":  "Story", "zoneCategory": "GW2" },
+    21: { "name": "Fields of Ruin", "type":  "Story", "zoneCategory": "GW2" },
+    22: { "name": "Fireheart Rise", "type":  "Story", "zoneCategory": "GW2" },
+    25: { "name": "Iron Marches", "type":  "Story", "zoneCategory": "GW2" },
+    32: { "name": "Diessa Plateau", "type":  "Story", "zoneCategory": "GW2" },
+    218: { "name": "Black Citadel", "type": "City", "zoneCategory": "GW2" },
+    51: { "name": "Straits of Devastation", "type":  "Story", "zoneCategory": "GW2" },
+    62: { "name": "Cursed Shore", "type":  "Story", "zoneCategory": "GW2" },
+    65: { "name": "Malchor's Leap", "type":  "Story", "zoneCategory": "GW2" },
+    15: { "name": "Queensdale", "type":  "Story", "zoneCategory": "GW2" },
+    17: { "name": "Harathi Hinterlands", "type":  "Story", "zoneCategory": "GW2" },
+    18: { "name": "Divinity's Reach", "type": "City", "zoneCategory": "GW2" },
+    23: { "name": "Kessex Hills", "type":  "Story", "zoneCategory": "GW2" },
+    24: { "name": "Gendarran Fields", "type":  "Story", "zoneCategory": "GW2" },
+    50: { "name": "Lion's Arch", "type": "City", "zoneCategory": "GW2" },
+    73: { "name": "Bloodtide Coast", "type":  "Story", "zoneCategory": "GW2" },
+    873: { "name": "Southsun Cove", "type":  "Story", "zoneCategory": "GW2" },
+    34: { "name": "Caledon Forest", "type":  "Story", "zoneCategory": "GW2" },
+    35: { "name": "Metrica Province", "type":  "Story", "zoneCategory": "GW2" },
+    54: { "name": "Brisban Wildlands", "type":  "Story", "zoneCategory": "GW2" },
+    91: { "name": "The Grove", "type": "City", "zoneCategory": "GW2" },
+    139: { "name": "Rata Sum", "type": "City", "zoneCategory": "GW2" },
+    39: { "name": "Mount Maelstrom", "type":  "Story", "zoneCategory": "GW2" },
+    53: { "name": "Sparkfly Fen", "type":  "Story", "zoneCategory": "GW2" },
+    988: { "name": "Dry Top", "type":  "Story", "zoneCategory": "GW2" },
+    1015: { "name": "The Silverwastes", "type":  "Story", "zoneCategory": "HoT" },
+
+    // Heart of Thorns
+    1041: { "name": "Dragon's Stand", "type": "Story", "zoneCategory": "HoT" },
+    1043: { "name": "Auric Basin", "type": "Story", "zoneCategory": "HoT" },
+    1045: { "name": "Tangled Depths", "type": "Story", "zoneCategory": "HoT" },
+    1052: { "name": "Verdant Brink", "type": "Story", "zoneCategory": "HoT" },
+    1068: { "name": "Gilded Hollow", "type": "Guild Hall", "zoneCategory": "HoT" },
+    1069: { "name": "Lost Precipice", "type": "Guild Hall", "zoneCategory": "HoT" },
+
+    // Living World Season 3
+    1165: { "name": "Bloodstone Fen", "type": "Living World: Season 3, Chapter 1", "zoneCategory": "HoT" },
+    1175: { "name": "Ember Bay", "type": "Living World: Season 3, Chapter 2", "zoneCategory": "HoT" },
+    1178: { "name": "Bitterfrost Frontier", "type": "Living World: Season 3, Chapter 3", "zoneCategory": "HoT" },
+    1185: { "name": "Lake Doric", "type": "Living World: Season 3, Chapter 4", "zoneCategory": "HoT" },
+    1195: { "name": "Draconis Mons", "type": "Living World: Season 3, Chapter 5", "zoneCategory": "HoT" },
+    1203: { "name": "Siren's Landing", "type": "Living World: Season 3, Chapter 6", "zoneCategory": "HoT" },
+
+    // Path of Fire
+    1210: { "name": "Crystal Oasis", "type": "Story", "zoneCategory": "PoF" },
 };
-var validMapIds = [26, 27, 28, 29, 30, 31, 326, 19, 20, 21, 22, 25, 32, 218, 51, 62, 65, 1203, 15, 17, 18, 23, 24, 50, 73, 873, 1185, 34, 35, 54, 91, 139, 39, 53, 1041, 1043, 1045, 1052, 1068, 1069, 1165, 988, 1015, 1210, 1175, 1195];
 
 
 var continents = {};
@@ -236,13 +242,15 @@ function getZoneSet(zonename) {
         "Zones": zoneLayer
     };
 
-    L.control.layers(baseMaps, overlayMaps, {collapsed: false}).addTo(map);
+    L.control.layers(baseMaps, overlayMaps, { collapsed: false }).addTo(map);
 
     // Load World Data
     getMergedFloorData().then(function (worldData) {
         _.forEach(worldData.regions, function (region) {
             _.forEach(region.maps, function (gameMap) {
-                if (_.indexOf(validMapIds, gameMap.id) !== -1) {
+                var localZoneData = worldZones[gameMap.id];
+                if (localZoneData !== undefined) {
+                    localZoneData.apiData = gameMap;
                     var newColor = getRandomColor();
                     var marker = null;
                     // Process POIs (Landmarks, Vistas, Waypoints)
@@ -353,7 +361,7 @@ function getZoneSet(zonename) {
 
                     var baseBounds = gameMap.continent_rect;
                     var bounds = [unproject(baseBounds[0]), unproject(baseBounds[1])];
-                    
+
                     var zonerect = L.rectangle(bounds, {
                         color: newColor,
                         weight: 1
