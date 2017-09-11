892b118157e28c271c54a50b1790f68fc1494825
Shawn McNaughton <shawngmc@gmail.com>
2017-09-10 23:43:44 -0400

    touch

--- ext/leaflet-markercluster/leaflet.markercluster-src.js
+++ ext/leaflet-markercluster/leaflet.markercluster-src.js
@@ -1,4 +1,4 @@
-/*
+ /*
  Leaflet.markercluster, Provides Beautiful Animated Marker Clustering functionality for Leaflet, a JS library for interactive maps.
  https://github.com/Leaflet/Leaflet.markercluster
  (c) 2012-2013, Dave Leaver, smartrak
