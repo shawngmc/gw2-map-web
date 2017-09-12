eca0306fff4d3a96e5d476c7759b8c4f79beb89d
Shawn McNaughton <shawngmc@gmail.com>
2017-09-12 11:32:51 -0400

    SW rewrite

--- js/mapdata-service-worker.js
+++ js/mapdata-service-worker.js
@@ -1,52 +1,63 @@
 // A list of local resources we always want to be cached.
+var CACHE_NAME = 'mapdata-cache';
 const PRECACHE_URLS = [
   '../data/zonedata.json',
   '../images'
 ];
 
-var CACHE = 'cache-and-update';
 
-self.addEventListener('install', function(evt) {
+
+self.addEventListener('install', function(event) {
   console.log('The service worker is being installed.');
   // Ask the service worker to keep installing until the returning promise resolves.
-  evt.waitUntil(precache());
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(PRECACHE_URLS);
+      })
+  );
 });
 
 
 // On fetch, use cache but update the entry with the latest contents from the server.
-self.addEventListener('fetch', function(evt) {
-  console.log('The service worker is serving the asset.');
-  // You can use respondWith() to answer immediately, without waiting for the network response to reach the service worker...
-  evt.respondWith(fromCache(evt.request));
-  // and waitUntil() to prevent the worker from being killed until the cache is updated.
-  evt.waitUntil(update(evt.request));
+self.addEventListener('fetch', function(event) {
+  console.log('The service worker is serving an asset.');
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response.
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
 });
-
-
-// Open a cache and use addAll() with an array of assets to add all of them to the cache. Return a promise resolving when all the assets are added.
-function precache() {
-  return caches.open(CACHE).then(function (cache) {
-      console.log('precaching');
-    return cache.addAll(PRECACHE_URLS);
-  });
-}
-
-//  Open the cache where the assets were stored and search for the requested resource. Notice that in case of no matching, the promise still resolves but it does with undefined as value.
-function fromCache(request) {
-      console.log('fromCache');
-  return caches.open(CACHE).then(function (cache) {
-    return cache.match(request).then(function (matching) {
-      return matching || Promise.reject('no-match');
-    });
-  });
-}
-
-// Update consists in opening the cache, performing a network request and storing the new response data.
-function update(request) {
-      console.log('update');
-  return caches.open(CACHE).then(function (cache) {
-    return fetch(request).then(function (response) {
-      return cache.put(request, response);
-    });
-  });
-}
