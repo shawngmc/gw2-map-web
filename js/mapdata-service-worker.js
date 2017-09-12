da19fff659d45a23f33dd6d4547da66e5234b392
Shawn McNaughton <shawngmc@gmail.com>
2017-09-12 11:19:58 -0400

    Updated SW cache methodology test

--- js/mapdata-service-worker.js
+++ js/mapdata-service-worker.js
@@ -1,55 +1,48 @@
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
-
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   '../data/zonedata.json'
 ];
 
-// The install handler takes care of precaching the resources we always need.
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
-  );
+var CACHE = 'cache-and-update';
+
+self.addEventListener('install', function(evt) {
+  console.log('The service worker is being installed.');
+  // Ask the service worker to keep installing until the returning promise resolves.
+  evt.waitUntil(precache());
 });
 
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
-  );
+
+// On fetch, use cache but update the entry with the latest contents from the server.
+self.addEventListener('fetch', function(evt) {
+  console.log('The service worker is serving the asset.');
+  // You can use respondWith() to answer immediately, without waiting for the network response to reach the service worker...
+  evt.respondWith(fromCache(evt.request));
+  // and waitUntil() to prevent the worker from being killed until the cache is updated.
+  evt.waitUntil(update(evt.request));
 });
 
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
-self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
 
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
-  }
-});
+// Open a cache and use addAll() with an array of assets to add all of them to the cache. Return a promise resolving when all the assets are added.
+function precache() {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.addAll(PRECACHE_URLS);
+  });
+}
+
+//  Open the cache where the assets were stored and search for the requested resource. Notice that in case of no matching, the promise still resolves but it does with undefined as value.
+function fromCache(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
+  });
+}
+
+// Update consists in opening the cache, performing a network request and storing the new response data.
+function update(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
+    });
+  });
+}
