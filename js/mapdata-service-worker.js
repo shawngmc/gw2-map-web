0c4dbc9741a82398929c0aa307fc62e6d41861a1
Shawn McNaughton <shawngmc@gmail.com>
2017-09-12 11:12:54 -0400

    Service Worker Path Fix

--- js/mapdata-service-worker.js
+++ js/mapdata-service-worker.js
@@ -3,7 +3,7 @@ const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
-  'data/zonedata.json'
+  '../data/zonedata.json'
 ];
 
 // The install handler takes care of precaching the resources we always need.
